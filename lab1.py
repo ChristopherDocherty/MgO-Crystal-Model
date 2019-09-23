99fea0b6d88bd438432fa0c8048fa7d8c21c728b
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2019-09-23 15:33:35 +0800

    Final version of lab script

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
-#Program to produce .xyz files for Sc, Bcc, Fcc, and Diamond crystalline
-#structures with the intent to display in VMD
+#Program to produce .xyz files for Sc, Bcc, Fcc, and
+#Diamond crystalline structures with the intent to display
+#in VMD
 
 import numpy as np
 
@@ -11,88 +12,151 @@ LatticeConstant = 5
 
 
 class sc():
-    '''
+    '''Creates an instance of a simple cubic crystal
+
+        Public methods:
+
+        extendUnitCell() -- takes in an atom and returns an np.array containing
+        atoms obtained by linear combination of the basis vectors within the
+        dimension of the lattice
+
+
+        Instance variables:
+
+        self.element -- The chemical symbol of the element crystal atoms are
+
+        self.structure -- The type of crystal structure, will change depending
+        on subclass
+
+        self.atoms -- An np.array containing all the atoms in the crystal. Each row is an atom while the columns are x,y and z postion respectively
+
+
+        Constructor arguments:
+
+        element -- The chemical symbol of the element crystal atoms are
+
+        dimensionOfLattice -- A tuple containing the dimensions of the lattice in terms of unit cells
+
+        a -- Lattice constant
 
     '''
 
     def __init__(self,element,dimensionOfLattice,a):
 
         self.element = element
+        self.structure = "Simple Cubic"
+        self.atoms = np.zeros((1,3))
 
-        self.atoms =  np.array([[0,0,0]])
+        extraAtoms = self.extendUnitCell(self.atoms,dimensionOfLattice,a)
 
-        extraAtoms = self.extendUnitCell(self.atoms,dimensionOfLattice,a,(0,0,0))
-        self.atoms = np.concatenate((self.atoms,extraAtoms), axis = 0)
+        self.atoms = extraAtoms
 
 
-    def extendUnitCell(self,atoms,dimensionOfLattice,a,repeatNum):
-        '''
+    def extendUnitCell(self,atoms,dimensionOfLattice,a):
+        ''' Returns all other similar atoms within the dimensions of the lattice
 
+
+            This method will take in a np.array of atoms and return those and  all other atoms with the same fractional coordinates which are within the dimensions of the lattice
+
+
+
+        Arguments:
+
+        atoms -- an np.array of atoms (see sc class docstring) to be extended
+
+        dimensionOfLattice -- A tuple containing the dimensions of the lattice in terms of unit cells
+
+        a -- Lattice constant
         '''
 
         unitVectors = [np.array([[a,0,0]]),np.array([[0,a,0]]),np.array([[0,0,a]])]
 
-        for length, unitVector, extraTime in zip(dimensionOfLattice,unitVectors,repeatNum):
+
+
+        for length, unitVector in zip(dimensionOfLattice,unitVectors):
             atomCount, __  = atoms.shape
 
-            if extraTime == 1:
-                length -= 1
-
-            for i in range(0,length):
+            for i in range(0,length-1):
+                #each time tempAtoms will be reading the atoms added in the
+                #previous iteration
                 tempAtoms = atoms[i*atomCount:(i+1)*atomCount,:] + unitVector
                 atoms = np.concatenate((atoms,tempAtoms),axis = 0)
+
 
         return atoms
 
 
 class bcc(sc):
+    '''Creates an instance of a body centred cubic crystal
+
+        Subclassed from sc and extends that class by adding the extra atoms in
+        a bcc unit cell
+
+    '''
 
     def __init__(self,element,dimensionOfLattice,a):
         super().__init__(element,dimensionOfLattice,a)
+        self.structure = "Body Centred Cubic"
+
+        #Hard coded bcc atom
         extraAtoms = 0.5*np.array([[a,a,a]])
-        extraAtoms = super().extendUnitCell(extraAtoms,dimensionOfLattice,a,(1,1,1))
+        extraAtoms = super().extendUnitCell(extraAtoms,dimensionOfLattice,a)
         self.atoms = np.concatenate((self.atoms,extraAtoms),axis = 0)
 
 
 
 class fcc(sc):
-    '''
+    '''Creates an instance of a face centred cubic crystal
+
+        Subclassed from sc and extends that class by adding the extra atoms in
+        a bcc unit cell
+
     '''
 
     def __init__(self,element,dimensionOfLattice,a):
         super().__init__(element,dimensionOfLattice,a)
-        extraAtoms = [[0,0.5*a,0.5*a],[0.5*a,0,0.5*a],[0.5*a,0.5*a,0]]
-        inDim = [(0,1,1),(1,0,1),(1,1,0)]
+        self.structure = "Face Centred Cubic"
 
-        for atoms, repeatNum in zip(extraAtoms,inDim):
-
-            atoms = np.array(atoms)
-            atoms = atoms[np.newaxis,:]
-            atoms = super().extendUnitCell(atoms,dimensionOfLattice,a,repeatNum)
-            self.atoms = np.concatenate((self.atoms,atoms),axis = 0)
+        #Hard coded fcc atoms
+        extraAtoms =np.array( [[0,0.5*a,0.5*a]+[0.5*a,0,0.5*a]+[0.5*a,0.5*a,0]])
+        extraAtoms = extraAtoms.reshape((-1,3))
+        extraAtoms = super().extendUnitCell(extraAtoms,dimensionOfLattice,a)
+        self.atoms = np.concatenate((self.atoms,extraAtoms),axis = 0)
 
 
 
 
 class diamond(fcc):
-    '''
+    '''Creates an instance of a diamond crystal
+
+        Subclassed from fcc and extends that class by adding the extra atoms in
+        a diamond unit cell
 
     '''
     def __init__(self,element,dimensionOfLattice,a):
 
         super().__init__(element,dimensionOfLattice,a)
+        self.structure = "Diamond"
 
+        #Hard coded diamond atoms
         extraAtoms = np.array([[0.25*a,0.25*a,0.25*a]+[0.25*a,0.75*a,0.75*a]+[0.75*a,0.75*a,0.25*a]+[0.75*a,0.25*a,0.75*a]])
         extraAtoms = extraAtoms.reshape((-1,3))
 
-        extraAtoms = super().extendUnitCell(extraAtoms,dimensionOfLattice,a,(1,1,1))
+        extraAtoms = super().extendUnitCell(extraAtoms,dimensionOfLattice,a)
         self.atoms = np.concatenate((self.atoms,extraAtoms),axis = 0)
 
 
 
 
 def writeToxyz(filename,crystal):
-    '''
+    ''' Takes some crystal class instance and writes its data to a .xyz file
+
+    Arguments:
+
+    filename -- Desired filename, should end in .xyz
+
+    crystal -- instance of any crystal type (i.e. sc class or subclass of sc)
+
 
     '''
     crystalFile = open(filename,"wt")
@@ -100,10 +164,9 @@ def writeToxyz(filename,crystal):
 
     atomCountFinal = crystal.atoms.shape[0]
 
-    #the (0,0,0) atom is always implicit
-    crystalFile.write("{0}\n".format(atomCountFinal+1) )
-    crystalFile.write("This is {0} \n".format(15))
-    crystalFile.write("{0:s} \t 0.000000000 \t 0.000000000 \t 0.000000000 \n".format(crystal.element))
+
+    crystalFile.write("{0}\n".format(atomCountFinal) )
+    crystalFile.write("This is a {0} {1} \n".format(dimensionOfLattice,crystal.structure))
     for i in range(0,atomCountFinal):
         crystalFile.write("{0:s} \t {1:0.9f} \t {2:0.9f} \t {3:0.9f} \n".format(crystal.element,crystal.atoms[i,0],crystal.atoms[i,1],crystal.atoms[i,2]))
 
